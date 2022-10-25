e6f186e3c5b1b18e3e63a189ee2456f1c54bbae0
RutujaPatil18 <prutuja189@gmail.com>
2022-10-25 23:21:19 +0530

    Add: Create addreessbook table for addressbook service

--- AddressBookSQL.sql
+++ AddressBookSQL.sql
@@ -4,6 +4,21 @@ create database addressbook;
 
 use addressbook;
 
+select database();
+
+create table addressbook(
+     id       int unsigned not null auto_increment,
+     first_name       varchar(10),
+     last_name        varchar(10),
+     address  varchar(100),
+     city     varchar(20),
+     state    varchar(20),
+     zip      int ,
+     phone_number     int,
+     email    varchar(30),
+     primary key (id)
+);
+
 show tables;
 
-select database();
+describe addressbook;
